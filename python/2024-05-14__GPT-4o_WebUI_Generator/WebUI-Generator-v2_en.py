a858d894553fb4d404bbc5266e95941cb2edcde1
Whykiki <info@whykiki.de>
2024-05-15 06:52:54 +0200

    Py Files ohne API Key

--- python/2024-05-14__GPT-4o_WebUI_Generator/WebUI-Generator-v2_en.py
+++ python/2024-05-14__GPT-4o_WebUI_Generator/WebUI-Generator-v2_en.py
@@ -8,7 +8,7 @@ Konfiguration / Settings
 """
 ############################################################################################################
 
-api_key = ''
+api_key = 'YOUR_API_KEY'
 
 #Standard endpoint for chat
 #Through model gpt-4o but without direct call to Vision API
